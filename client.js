c45e042693caa02035a77870062c0e122e26d89d
Kajal Pandey <123621691+kajalpandey25@users.noreply.github.com>
2023-09-29 21:51:27 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,31 @@
+const  socket = io('http://localhost:8000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector(".container")
+
+const append = (message, position)=>{
+    const messageElement =document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('messge')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement);
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append('You: ${message}', 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+})
+const name = prompt ("Enter your name to join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', data =>{
+    append('${name} joined the chat','right')
+
+})
+socket.on('receive', data=>{
+    append('${data.message}: ${data.user}','left')
+})
